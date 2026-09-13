3dc43faf67763287c1cf06cf750df2256d25308c
Nikhil Aghi <nikhilaghi@gmail.com>
2026-09-13 13:57:45 +0530

    Fix real inspection results flow

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { 
   ArrowLeft, 
